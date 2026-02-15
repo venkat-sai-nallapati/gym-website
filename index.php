97e4746a782f0a6fd55f8f9abeece909e8d94558
venkat-sai-nallapati <venkatsainallapati@gmail.com>
2026-02-15 12:10:58 +0530

    created release branch and updated index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ session_start();
 <title>home</title>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
+    <!-- Branch release -->
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
